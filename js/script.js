9b5dc7ce64570d4f46d66597d5e2279a8142e73b
jvollherbst <jasmine.cardoza@gmail.com>
2016-02-03 21:16:28 -0500

    working out how to recognize user click patters

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function displayImg(){//Should be a constructor function
   var imgArrayOne = ['images/blue-square.svg', 'images/red-circle.svg', 'images/yellow-triangle.svg'];
 
   for(var i = 0; i < imgArrayOne.length; i++){
-    $('<div>').addClass('new-img').appendTo('#main-img').prepend('<img src="' + imgArrayOne[i] + '"/>');
+    $('<div>').addClass('new-img ' + [i]).appendTo('#main-img').prepend('<img src="' + imgArrayOne[i] + '"/>');
   }
 }//end displayImg
 
@@ -29,18 +29,17 @@ displayImg();
 
 function hideImg(){
   setTimeout(function(){
-    $('.new-img').hide();
+    $('#main-img').empty();
   }, 4000);
 }//end hideImg
 
 hideImg();
 
-// Source: https://css-tricks.com/snippets/javascript/shuffle-array/
+// Source: CHRIS COYIER https://css-tricks.com/snippets/javascript/shuffle-array/
 function Shuffle(o) {
 	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 	return o;
-};
-// Source: https://css-tricks.com/snippets/javascript/shuffle-array/
+};// Source: CHRIS COYIER https://css-tricks.com/license/
 
 
 function shuffleArray(){
@@ -51,7 +50,7 @@ function shuffleArray(){
   var shuffledArray = Shuffle(imgArrayOne);
 
   for(var i = 0; i < shuffledArray.length; i++){
-    $('<div>').addClass('choice-img').appendTo('#main-img').prepend('<img src="' + shuffledArray[i] + '"/>');
+    $('<div>').addClass('choice-img ' + [i]).appendTo('#main-img').prepend('<img src="' + shuffledArray[i] + '"/>');
   }
 }, 6000);
 }
@@ -59,6 +58,7 @@ shuffleArray();
 
 function playerMove(){
   //should check user's onclicks and match against source array
+  
 }
 
 
