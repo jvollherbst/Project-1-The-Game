7f61aea52a0fa44f93cf992e2080261c4a18f3be
jvollherbst <jasmine.cardoza@gmail.com>
2016-02-07 12:42:32 -0500

    organized functions into a game object and game now loops and can restart

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var arrayLibrary = [
-  ['images/blue-square.svg', 'images/red-circle.svg', 'images/yellow-triangle.svg'],
-  // ['images/blue-square.svg', 'images/red-circle.svg', 'images/yellow-triangle.svg', 'images/green-diamond.svg'],
-  // ['images/green-diamond.svg', 'images/yellow-triangle.svg','images/red-circle.svg','images/blue-square.svg']
+  ['images/blue-square.svg', 'images/red-circle.svg', 'images/yellow-triangle.svg', 'images/green-diamond.svg'],
+  ['images/blue-square.svg', 'images/red-circle.svg', 'images/yellow-triangle.svg', 'images/green-diamond.svg'],
+  ['images/green-diamond.svg', 'images/yellow-triangle.svg','images/red-circle.svg','images/blue-square.svg']
 ];
 
 var randomArray = arrayLibrary[Math.floor(Math.random() * arrayLibrary.length)];
@@ -20,121 +20,142 @@ var playerOneScore = 0;
 var playerTwoScore = 0;
 
 var currentPlayer = 'Player One';
+var gameOver = false;
 
 $(document).ready(function() {
 
-function displayImg(){
-  var imgArrayOne = randomArray;
-  for(var i = 0; i < imgArrayOne.length; i++){
-    var x =  $('<div>').addClass('new-img').prepend('<img src="' + imgArrayOne[i] + '"/>').attr('data-value', i);
-    winState.push(x.attr('data-value'));
-    x.appendTo('#main-img');
-  };
-  hideImg()
-  shuffleShowImg()
-};//end displayImg
+var game = {
+  displayImg: function(){
+    var imgArrayOne = randomArray;
+    for(var i = 0; i < imgArrayOne.length; i++){
+      var x =  $('<div>').addClass('new-img').prepend('<img src="' + imgArrayOne[i] + '"/>').attr('data-value', i);
+      winState.push(x.attr('data-value'));
+      x.appendTo('#main-img');
+    };
+    game.hideImg()
+    game.shuffleShowImg()
+    game.playerMove();
 
-function hideImg(){
-  setTimeout(function(){
-    $('.new-img').hide();
-  }, 3000);//needs to be less than shuffleShowImg
-};//end hideImg
-// hideImg();
+  },//end displayImg
 
-function shuffleShowImg(){
-  setTimeout(function(){
-    var $newImg = $('.new-img');
-    var shuffled = Shuffle($newImg);
-    for(var i = 0; i < shuffled.length; i++){
-      $('#main-img').prepend(shuffled[i]);
-    //  $(shuffled[i]).click(function(event) { //this includes the #document as part of the array.stopPropagation not working...
-    //     playerMove(event.target)
-    //   });
+  hideImg: function(){
+    setTimeout(function(){
+      $('.new-img').hide();
+    }, 3000);//needs to be less than shuffleShowImg
+  },//end hideImg
+
+  shuffleShowImg: function(){
+    setTimeout(function(){
+      var $newImg = $('.new-img');
+      var shuffled = Shuffle($newImg);
+      for(var i = 0; i < shuffled.length; i++){
+        $('#main-img').prepend(shuffled[i]);
+      //  $(shuffled[i]).click(function(event) { //this includes the #document as part of the array.stopPropagation not working...
+      //     playerMove(event.target)
+      //   });
+      }
+      $($newImg).show();
+    }, 3000);
+  },//end shuffleShowImg
+
+  playerMove: function (target){
+    $('.new-img').click(function(e){
+      playerInput.push($(this).attr('data-value'));
+
+      if(playerInput.length === winState.length){
+        return game.checkWin()
+      }
+    });
+  },
+
+  checkWin: function (){
+
+    var status = false;
+
+    //Had help with this from Ari Ingber
+    for(var i = 0; i < winState.length; i++){
+      if(winState[i] === playerInput[i]){
+      status = true;
     }
-    $($newImg).show();
-  }, 3000);
-};//end shuffleShowImg
-// shuffleShowImg();
-
-function playerMove(target){
-  $('.new-img').click(function(e){
-    playerInput.push($(this).attr('data-value'));
-
-    if(playerInput.length === winState.length){
-      return checkWin()
+    else{
+      status = false;
+      break;
     }
-  });
-};
-// playerMove();
+    };
 
-function checkWin(){
+    winState = [];
+    playerInput = [];
 
-  var status = false;
+    if(status == true && currentPlayer === 'Player One'){
+      playerOneScore ++
+      $('.playerone-score').text(playerOneScore)
+      // alert('Player One You Win')
+    }
+    else if(status == false && currentPlayer === 'Player One'){
+      alert('That is incorrect Player One')
+    }
+    else if(status == true && currentPlayer === 'Player Two'){
+      playerTwoScore ++
+      $('.playertwo-score').text(playerTwoScore)
+      // alert('Player Two You Win')
+    }
+    else if(status == false && currentPlayer === 'Player Two'){
+      alert('That is incorrect Player Two')
+    }
+    game.newPlayer()
+  },
 
-  //Had help with this from Ari Ingber
-  for(var i = 0; i < winState.length; i++){
-    if(winState[i] === playerInput[i]){
-    status = true;
-  }
-  else{
-    status = false;
-    break;
-  }
-  };
+  newPlayer: function(){
+    if(currentPlayer === 'Player One'){
+      currentPlayer = 'Player Two';
+      alert('Player Two\'s turn')
+    }
+    else{
+      currentPlayer = 'Player One';
+      alert('Player One\'s turn')
+    }
+    game.nextRound();
+  },
 
-  winState = [];
-  playerInput = [];
+  nextRound: function(){
+    if(playerOneScore === 3 || playerTwoScore === 3){
+      game.finalWinner();
+    }
+    else{
+      $('#main-img').empty();
+      game.displayImg();
+    }
+  },
 
-  if(status == true && currentPlayer === 'Player One'){
-    playerOneScore ++
+  finalWinner: function(){
+
+    if(playerOneScore === 3 || playerTwoScore === 3){
+      gameOver = true;
+      alert('Game Over')
+      $('#main-img').empty();
+
+      if(playerOneScore > playerTwoScore){
+        alert('Player One Wins')
+      }
+      else if(playerTwoScore > playerOneScore){
+        alert('Player Two Wins')
+      }
+      else if(playerOneScore === playerTwoScore){
+        alert('It\'s a tie')
+      }
+      game.resetGame();
+    }
+  },
+
+  resetGame: function(){
+    playerOneScore = 0;
     $('.playerone-score').text(playerOneScore)
-    // alert('Player One You Win')
-  }
-  else if(status == false && currentPlayer === 'Player One'){
-    alert('That is incorrect Player One')
-  }
-  else if(status == true && currentPlayer === 'Player Two'){
-    playerTwoScore ++
+
+    playerTwoScore = 0;
     $('.playertwo-score').text(playerTwoScore)
-    // alert('Player Two You Win')
-  }
-  else if(status == false && currentPlayer === 'Player Two'){
-    alert('That is incorrect Player Two')
-  }
 
-  if(playerOneScore === 1 || playerTwoScore === 1){
-    alert('Game Over')
-    if(playerOneScore > playerTwoScore){
-      alert('Player One Wins')
-    }
-    else if(playerTwoScore > playerOneScore){
-      alert('Player Two Wins')
-    }
-    else if(playerOneScore === playerTwoScore){
-      alert('It\'s a tie')
-    }
-    $('#main-img').remove();
-  }
-  newPlayer()
-};
-// checkWin()
-
-function newPlayer(){
-  if(currentPlayer === 'Player One'){
-    currentPlayer = 'Player Two';
-    alert('Player Two\'s turn')
-  }
-  else{
     currentPlayer = 'Player One';
-    alert('Player One\'s turn')
   }
-  nextRound();
-};
-
-function nextRound(){
-  $('#main-img').empty();
-  displayImg()
-  playerMove()
 };
 
 //Razaik Boparai's timer code
@@ -150,8 +171,7 @@ function nextRound(){
 // displayTime();
 
 function startGame(){
-  displayImg()
-  playerMove()
+  game.displayImg();
 };
 
 function start(){
